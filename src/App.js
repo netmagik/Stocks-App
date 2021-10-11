6e6885e710c82b72fe67b70d2707b7b1938eea6e
Irina <irina@netmagik.com>
2021-10-11 16:16:11 -0400

    Get Data for X and Y coordinates

--- src/App.js
+++ src/App.js
@@ -5,22 +5,42 @@ import Search from "./components/search";
 function App() {
 
   const [query, setQuery] = useState('');
-  const [data, setData] = useState({});
+  const [stockChartXValues, setStockChartXValues] = useState([]);
+  const [stockChartYValues, setStockChartYValues] = useState([]);
+
 
   const key = process.env.REACT_APP_API_KEY;
 
   const search = async (e) => {
+    const stockChartXValues = [];
+    const stockChartYValues = [];
     if (e.key === 'Enter') {
-      try {
+
         const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${query}&apikey=${key}`;
-        const res = await fetch(url);
-        const json = await res.json();
-        console.log(json);
-        setData(json);
-    }
-    catch (error) {
-        console.log(error);
-    }
+        await fetch(url)
+        .then(res => res.json())
+        .then((data) => {
+          console.log(data['Time Series (Daily)']);
+      
+          // console.log(data.filter(obj => obj['Time Series (Daily)']))
+          // Object.keys(data.map(key => key['Time Series (Daily)']['2. high']))
+          for (let key in data['Time Series (Daily)']) {
+            stockChartYValues.push(data['Time Series (Daily)'][key]['2. high']);
+            stockChartXValues.push(key);
+          }
+          setStockChartXValues(stockChartXValues.reverse());
+          setStockChartYValues(stockChartYValues.reverse());
+          console.log(stockChartXValues);
+          console.log(stockChartYValues)
+        })
+        
+        // const json = await res.json();
+        // console.log(stockHighValues);
+        // setStocks(stockHighValues);
+
+    // catch (error) {
+    //     console.log(error);
+    // }
     }
   }
 
---
+++ src/components/Stock.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Line } from 'react-chartjs-2';
+
+const Stock = (props) => {
+    return (
+        'Chart will go here'
+    )
+}
+
+export default Stock;
