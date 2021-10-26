2eb71e0e22c681ed48ca4d037165cf120f581790
Irina <irina@netmagik.com>
2021-10-26 16:08:10 -0400

    Rewrite as try-catch instead of then

--- src/App.js
+++ src/App.js
@@ -23,29 +23,49 @@ function App() {
 
     if (e.key === 'Enter') {
 
-        const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${query}&apikey=${key}`;
-        await fetch(url)
-        .then(res => res.json())
-        .then((data) => {
-          console.log(data['Time Series (Daily)']);
+      try {
+        const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${query}&apikey=${key}`);
+        const data = await response.json();
+        console.log(data);
+        const stockData = data['Time Series (Daily)'];
+        console.log(stockData);
+        for (let key in stockData) {
+          stockChartXValues.push(key);
+          stockChartYValuesHigh.push(stockData[key]['2. high']);
+          stockChartYValuesLow.push(stockData[key]['3. low']);
+          stockChartYValuesOpen.push(stockData[key]['1. open']);
+        }
+        // setQuery('');
+        setStockChartXValues(stockChartXValues.reverse());
+        setStockChartYValuesHigh(stockChartYValuesHigh.reverse());
+        setStockChartYValuesLow(stockChartYValuesLow.reverse());
+        setStockChartYValuesOpen(stockChartYValuesOpen.reverse());
+      } catch (error) {
+        console.log(error);
+        // const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${query}&apikey=${key}`;
+        // await fetch(url)
+        // .then(res => res.json())
+        // .then((data) => {
+        //   console.log(data['Time Series (Daily)']);
       
-          for (let key in data['Time Series (Daily)']) {
-            stockChartYValuesHigh.push(data['Time Series (Daily)'][key]['2. high']);
-            stockChartXValues.push(key);
-            stockChartYValuesOpen.push(data['Time Series (Daily)'][key]['1. open']);
-            stockChartYValuesLow.push(data['Time Series (Daily)'][key]['3. low']);
+        //   for (let key in data['Time Series (Daily)']) {
+        //     stockChartYValuesHigh.push(data['Time Series (Daily)'][key]['2. high']);
+        //     stockChartXValues.push(key);
+        //     stockChartYValuesOpen.push(data['Time Series (Daily)'][key]['1. open']);
+        //     stockChartYValuesLow.push(data['Time Series (Daily)'][key]['3. low']);
 
-          }
-          setStockChartXValues(stockChartXValues.reverse());
-          setStockChartYValuesHigh(stockChartYValuesHigh.reverse());
-          setStockChartYValuesOpen(stockChartYValuesOpen.reverse());
-          setStockChartYValuesLow(stockChartYValuesLow.reverse());
-          // setQuery('');
-          console.log(stockChartXValues);
-          console.log(stockChartYValuesHigh)
-        })
+        //   }
+        //   setStockChartXValues(stockChartXValues.reverse());
+        //   setStockChartYValuesHigh(stockChartYValuesHigh.reverse());
+        //   setStockChartYValuesOpen(stockChartYValuesOpen.reverse());
+        //   setStockChartYValuesLow(stockChartYValuesLow.reverse());
+        //   // setQuery('');
+        //   console.log(stockChartXValues);
+        //   console.log(stockChartYValuesHigh)
+        //})
     }
   }
+}
 
   const handleSearch = (symbol) => {
     console.log('Set Query');
